366a4e41de194c498c962c540075b1c6824c355f
ygautomo <y_gautomo@yahoo.com>
2018-05-05 16:03:33 +0700

    Create helloworld.js
    Create helloworld.js

---
+++ helloworld.js
@@ -0,0 +1,16 @@
+/*
+ * Title: Basic Node Example
+ * Description: Hello World.
+ * Author: Yugo Gautomo
+ * Date: 05/01/18
+ *
+ */
+
+// Dependencies
+
+// App object
+
+// Configuration
+
+// Command Hello World
+console.log("Hello world!");
