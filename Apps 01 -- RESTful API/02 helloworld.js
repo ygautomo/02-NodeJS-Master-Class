418e824f9f47cc341d477397f82c223ffc45819c
ygautomo <y_gautomo@yahoo.com>
2019-06-02 00:39:03 +0700

    Update several files

--- Apps 01 -- RESTful API/02 helloworld.js
+++ Apps 01 -- RESTful API/02 helloworld.js
@@ -6,10 +6,12 @@ Author: Yugo Gautomo
 Date: May 01, 2019
 */
 
-console.log('Hello World');
-console.log('Node.js Master Class');
-console.log('Welcome to Node.js');
-console.log('Module: 01 Basic Scaffolding');
+console.log("Hello World");
+console.log("Node.js Master Class");
+console.log("Welcome to Node.js");
+console.log("Module: 0102 Basic Scaffolding");
+console.log("Try to write in multi line
+	strings");
 
 // Running command
 // cd ./Apps 01 -- RESTful API
---
+++ Apps 01 -- RESTful API/06 querystrings.js
@@ -0,0 +1,46 @@
+/*
+06 requestmethods.js
+Module 0106 Parsing query strings
+
+Author: Yugo Gautomo
+Date: May 01, 2019
+*/
+
+
+// dependencies
+const http = require('http');
+const url = require('url');
+
+// The server should response to all requests with a string
+const server = http.createServer(function(req, res) {
+	// Get the URL and parse it
+	let parsedUrl = url.parse(req.url,true);
+	
+	// Get the path
+	let path = parsedUrl.pathname;
+	let trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+	// Get the HTTP methods
+	let method = req.method.toUpperCase();
+
+	// Get the query strings as an object
+	let queryStringObject = parsedUrl.query;
+
+	// Send the response
+	res.end("Hello World\n");
+
+	// Log the request path
+	console.log("Request received on path:", trimmedPath, "with method:", method, 
+		"and with these query string parameters", queryStringObject);
+});
+
+// Start the server, and have it listen on port 3000
+server.listen(3000, function() {
+	console.log("The server is listening on port 3000 now");
+})
+
+// Running command
+// cd ./Apps 01 -- RESTful API
+// node 06 querystrings.js
+// http://45.77.41.41:3000/path
+// curl localhost:3000/path
